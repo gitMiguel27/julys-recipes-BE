70a54af9d096c761de16597d2a0686dad854a77d
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-01-26 10:06:37 -0500

    intialized backend

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const port = process.env.PORT || 3000
+const cors = require('cors')
+const express = require('express')
+const app = express()
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello Backend')
+})
+
+// Catch ALL route
+app.get('*', (req, res) => {
+    res.send('Invalid Route: 404')
+})
+
+//Listener
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}...`)
+})
