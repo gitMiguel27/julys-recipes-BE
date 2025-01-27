7bb608312e7906981838905d8aec3b016a7afd45
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-01-27 14:15:09 -0500

    seed route working

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const port = process.env.PORT || 3000
 const cors = require('cors')
 const express = require('express')
 const connectDB = require('./config/db')
+// Recipe routes, seeds, and model
 const recipeRoutes = require('./routes/recipeRoutes')
+const Recipe = require('./models/recipeSchema')
+const starterRecipes = require('./config/recipe_seed')
 
 const app = express()
 
@@ -19,6 +22,20 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/recipes', recipeRoutes)
+
+app.get('/api/seed', async (req, res) => {
+    try {
+        console.log('Seeding data...')
+        await Recipe.deleteMany({})
+        let recipeSeedData = await Recipe.create(starterRecipes)
+
+        res.json([
+            recipeSeedData,
+        ])
+    } catch (error) {
+        console.log(`Something went wrong loading seed data: ${error.message}`)
+    }
+})
 
 // Catch ALL route
 app.get('*', (req, res) => {
