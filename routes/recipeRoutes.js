2cd7fdfeeb2253168e8bf23df8325c907f4d53ef
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2025-02-03 12:11:27 -0500

    deleted get single recipe because not needed

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -21,15 +21,6 @@ router.post('/', async (req, res) => {
     }
 })
 
-// router.get('/:id', async (req, res) => {
-//     try {
-//         let singleRecipe = await Recipe.findById(req.params.id)
-//         res.json(singleRecipe)
-//     } catch (error) {
-//         res.status(500).json({ error: error.message })
-//     }
-// })
-
 router.put('/:id', async (req, res) => {
     try {
         let updatedRecipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true, })
